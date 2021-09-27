e8a45fd1a099388b9b713f53c851f762da3e3336
Dyshko <dyshko2012@yandex.by>
2021-09-27 22:19:01 +0300

    podnyat web i BD

---
+++ src/main/java/my/company/controllers/UserController.java
@@ -0,0 +1,36 @@
+package my.company.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+//    private final UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/users")
+//    public String getUsers(Model model) {
+//        model.addAttribute("user", userService.selectAll());
+//        return "views/users";
+//    }
+//
+//    @GetMapping(value = "/add")
+//    public String getPageSave(Model model, User user) {
+//        model.addAttribute(user);
+//        return "views/userForm";
+    }
+//
+//    @PostMapping("saveUser")
+//    public String saveUsers(@RequestParam Integer id, @RequestParam String firstName,
+//                            @RequestParam String lastName, @RequestParam Integer age,
+//                            @RequestParam Adress adress) {
+//        User user = new User(id, firstName, lastName, age, adress);
+//        userService.addUser(user);
+//        return "Users";
+//    }
+//
+//
+//}
+
