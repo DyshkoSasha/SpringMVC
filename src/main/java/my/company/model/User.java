7b51969a1dcb1df2fa6a087a87d66430dd2d0328
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-10-04 17:35:00 +0000

    Update User.java

--- src/main/java/my/company/model/User.java
+++ src/main/java/my/company/model/User.java
@@ -43,7 +43,7 @@ public class User {
     }
 
     @Override
-    public String toString() {
+    public String toString() {//todo нашо яно таби?
         return "User{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
